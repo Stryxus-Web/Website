20ee6aca357e854ff1fbf39a609b29ea8f34c9d5
Connor Shearer <tech-seven0q@icloud.com>
2023-07-12 17:28:48 +0100

    Remove the setups page for a remake

--- Client/wwwroot-dev/assets.js
+++ Client/wwwroot-dev/assets.js
@@ -10,37 +10,18 @@ import './ts/navigationBar.ts'
 //
 
 // PNG
-import './img/avatar.png?as=avif'
-import './img/cutout.png?as=avif'
+import './img/avatar.png'
+import './img/cutout.png'
 
 // PNG - Home
-import './img/home/background-0.png?as=avif'
-import './img/home/setup-pc-0.png?as=avif'
-import './img/home/setup-pc-1.png?as=avif'
-import './img/home/setup-pc-2.png?as=avif'
-import './img/home/setup-pc-3.png?as=avif'
-import './img/home/setup-imac-0.png?as=avif'
-import './img/home/setup-imac-1.png?as=avif'
-import './img/home/setup-monitor-0.png?as=avif'
-import './img/home/setup-monitor-1.png?as=avif'
-import './img/home/setup-audio-0.png?as=avif'
-import './img/home/setup-audio-1.png?as=avif'
-import './img/home/setup-keyboardmouse-0.png?as=avif'
-import './img/home/setup-keyboardmouse-1.png?as=avif'
-import './img/home/setup-otherperi-0.png?as=avif'
-import './img/home/setup-desk-0.png?as=avif'
-import './img/home/setup-phonetablet-0.png?as=avif'
-import './img/home/setup-phonetablet-1.png?as=avif'
-import './img/home/setup-phonetablet-2.png?as=avif'
-import './img/home/setup-serversbc-0.png?as=avif'
-import './img/home/setup-serversbc-1.png?as=avif'
+import './img/home/background-0.png'
 
 // PNG - Media
-import './img/media/background-0.png?as=avif'
-import './img/media/background-1.png?as=avif'
-import './img/media/background-2.png?as=avif'
-import './img/media/background-3.png?as=avif'
-import './img/media/background-4.png?as=avif'
-import './img/media/background-5.png?as=avif'
+import './img/media/background-0.png'
+import './img/media/background-1.png'
+import './img/media/background-2.png'
+import './img/media/background-3.png'
+import './img/media/background-4.png'
+import './img/media/background-5.png'
 
 //
