378d81464314faea150ab7b63774a20523ab6c67
Connor Shearer <tech-seven0q@icloud.com>
2023-08-29 11:28:11 +0100

    Add basic Discord Bot functionality

---
+++ Modules/Stryxus.Module.Discord/StryxBot.cs
@@ -0,0 +1,28 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stryxus.Module.Discord;
+
+public class StryxBot
+{
+    public void Connect(string discordToken)
+    {
+        DiscordSocketClient client = new(new DiscordSocketConfig() 
+        {
+            AlwaysDownloadUsers = true,
+            GatewayIntents = GatewayIntents.Guilds | GatewayIntents.AutoModerationActionExecution | GatewayIntents.DirectMessages,
+        });
+        client.LoginAsync(TokenType.Bot, discordToken);
+        client.StartAsync();
+        client.Log += async (LogMessage msg) => 
+        {
+            Console.WriteLine($"StryxBot: {msg.Message}");
+        };
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,10 +1,13 @@
 // The website will be proxied by Cloudflare so, no need to add some things like security headers.
 
 using Microsoft.AspNetCore.StaticFiles;
+using Stryxus.Module.Discord;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
+builder.Services.AddSingleton<StryxBot>();
 
 FileExtensionContentTypeProvider provider = new();
 provider.Mappings[".avif"] = "image/avif";
@@ -24,6 +27,10 @@ app.UseStaticFiles();
 app.UseRouting();
 app.MapRazorPages();
 app.MapFallbackToFile("index.html");
+
+string? discordToken;
+if ((discordToken = app.Configuration["discord_token"]) is not null) app.Services.GetService<StryxBot>()?.Connect(discordToken);
+
 #if DEBUG
 await app.RunAsync("https://0.0.0.0:7076");
 #else
