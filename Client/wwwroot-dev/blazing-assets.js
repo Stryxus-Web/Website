bcdd60ce5b6c1d9d6d4503cba2c886c096bf7175
Connor Shearer <11709044+Stryxus@users.noreply.github.com>
2023-11-13 11:00:22 +0000

    Additional file changes for Blazing

--- Client/wwwroot-dev/blazing-assets.js
+++ Client/wwwroot-dev/blazing-assets.js
@@ -12,33 +12,18 @@ import './bundle'
 import './favicon.png'
 
 // PNG
-import './img/avatar.png?as=avif'
-import './img/avatar.png?as=webp'
-
-import './img/cutout.png?as=avif'
-import './img/cutout.png?as=webp'
+import './img/avatar.png'
+import './img/cutout.png'
 
 // PNG - Home
-import './img/home/back_main.png?as=avif'
-import './img/home/back_main.png?as=webp'
+import './img/home/back_main.png'
 
 // PNG - Media
-import './img/media/background-0.png?as=avif'
-import './img/media/background-0.png?as=webp'
-
-import './img/media/background-1.png?as=avif'
-import './img/media/background-1.png?as=webp'
-
-import './img/media/background-2.png?as=avif'
-import './img/media/background-2.png?as=webp'
-
-import './img/media/background-3.png?as=avif'
-import './img/media/background-3.png?as=webp'
-
-import './img/media/background-4.png?as=avif'
-import './img/media/background-4.png?as=webp'
-
-import './img/media/background-5.png?as=avif'
-import './img/media/background-5.png?as=webp'
+import './img/media/background-0.png'
+import './img/media/background-1.png'
+import './img/media/background-2.png'
+import './img/media/background-3.png'
+import './img/media/background-4.png'
+import './img/media/background-5.png'
 
 //
