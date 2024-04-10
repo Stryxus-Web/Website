ddc96e198901bed7c76b40d47ab6451326495de4
Stryxus <11709044+Stryxus@users.noreply.github.com>
2024-04-10 16:31:38 +0100

    Revert "SSR production fixes"
    This reverts commit 2aac846adacc252b8068b0f8d5d5a296b0ce0f55.

--- server.ts
+++ server.ts
@@ -18,7 +18,6 @@ const port = process.env.PORT || 7076;
 
 const app = express();
 let server: HttpsServer<any, any> | Server<any, any>; 
-let templateRenderObject = "<!--SSR-->";
 
 if (isDev) {
   const vite: ViteDevServer = await createViteServer();
@@ -29,7 +28,7 @@ if (isDev) {
       const template = await vite.transformIndexHtml(url, fs.readFileSync("index.html", "utf-8"));
       const { prerender } = await vite.ssrLoadModule("./src/entry-server.tsx");
     
-      const html = template.replace(templateRenderObject, prerender);
+      const html = template.replace(`<!--SSR-->`, prerender);
       res.status(200).set({ "Content-Type": "text/html" }).end(html);
     } catch (error) {
       debug(error);
@@ -47,9 +46,9 @@ if (isDev) {
   app.use("*", async (_, res) => {
     try {
       const template = fs.readFileSync("./dist/client/index.html", "utf-8");
-      const { prerender } = await import("./dist/server/entry-server.js");
+      const { prerender } = await import("./dist/server/entry-server.tsx");
    
-      const html = template.replace(templateRenderObject, prerender);
+      const html = template.replace(`<!--outlet-->`, prerender);
       res.status(200).set({ "Content-Type": "text/html" }).end(html);
     } catch (error) {
       debug(error);
