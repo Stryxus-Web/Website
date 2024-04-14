afe06655e258cfa181ef9c69a7c363600d801480
Stryxus <11709044+Stryxus@users.noreply.github.com>
2024-04-14 13:23:20 +0100

    Add styled components todo

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,7 @@ import Img_Avatar from "./assets/img/avatar.png";
 // TODO: Dont rely on gsap to set the mobile nav/header/footer style
 // TODO: Redesign home page
 // TODO: Fix bug which doesnt close the navbar when selecting a page on mobile
+// TODO: Integrate styled components with SASS
 
 declare global {
 	interface Window {
