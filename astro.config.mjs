d2d2021462e2d1cd140573ec3e3e062d68ba68c0
Stryxus <11709044+Stryxus@users.noreply.github.com>
2024-07-15 16:59:32 +0100

    Setup the fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -39,7 +39,10 @@ export default defineConfig({
       target: "es2022",
       emptyOutDir: true
     },
-    plugins: [tsconfigPaths(), webfontDownload(["https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"], {
+    plugins: [tsconfigPaths(), webfontDownload([
+      "https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&display=swap", 
+      "https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+    ], {
       injectAsStyleTag: true,
       minifyCss: true,
       async: true,
